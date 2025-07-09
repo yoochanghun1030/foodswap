aa187875ec3c165c0559412fedbff875c199eff9
changhun yoo <yoochanghun90@gmail.com>
2025-07-09 13:20:07 +0100

    create page midify

--- frontend/app/exchange/create/page.tsx
+++ frontend/app/exchange/create/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+export const dynamic = "force-dynamic";
 import { useState, useEffect } from 'react';
 import { useSearchParams } from 'next/navigation';
 import axios from '@/utils/axios';
