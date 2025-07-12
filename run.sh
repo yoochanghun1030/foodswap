a6bf3c3204bb9596edefbf1529cffff9748f817d
changhun yoo <yoochanghun90@gmail.com>
2025-07-12 09:38:42 +0100

    Fix: add Dockerfile and build .jar

--- run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-chmod +x mvnw
-./mvnw spring-boot:run
--- src/main/java/org/example/feaswap/security/SecurityConfig.java
+++ src/main/java/org/example/feaswap/security/SecurityConfig.java
@@ -67,7 +67,6 @@ public class SecurityConfig {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
         config.setAllowedOrigins(List.of("http://localhost:3000"));
-        config.setAllowedOrigins(List.of("https://foodswapapp.vercel.app"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT","PATCH", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("Authorization", "Content-Type", "X-Requested-With"));
 
